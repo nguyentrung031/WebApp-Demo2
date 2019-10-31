0b5bfe2a63aeb5ca61f2e70e17a2c8558a4375f8
nguyentrung031 <trung9977.mta@gmail.com>
2019-10-31 16:13:25 +0700

    #2-edit-indent-4

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,14 @@ const express = require('express')
 var bodyParser = require('body-parser');
 const app = express()
 const Data = require('./Data/Data')
- 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
-	res.send('hello from server!')
+  res.send('hello from server!')
 })
 app.get('/api/Tendency', (req, res) => {
-	res.json(Data);
+  res.json(Data);
 });
 app.listen(5000, () => {
-	console.log('App listening on port 5000')
+  console.log('App listening on port 5000')
 })
