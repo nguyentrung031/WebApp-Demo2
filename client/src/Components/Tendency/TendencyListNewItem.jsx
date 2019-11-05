f01474b440e361faab07e7c6d79835364c1da798
nguyentrung031 <trung9977.mta@gmail.com>
2019-11-05 16:48:13 +0700

    fix indent

--- client/src/Components/Tendency/TendencyListNewItem.jsx
+++ client/src/Components/Tendency/TendencyListNewItem.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 class TendencyListNewItem extends Component {
-	render() {
-		return (
-			<div className="card-item">
+  render() {
+    return (
+      <div className="card-item">
         <div className="img-3">
           <a href={`/Detail/${this.props.Id}`}>
             <img src={this.props.Image} alt={this.props.Name} />
@@ -14,8 +14,8 @@ class TendencyListNewItem extends Component {
           <h6>{this.props.Name}</h6>
         </div>
       </div>
-		);
-	}
+    );
+  }
 }
 
 export default TendencyListNewItem;
