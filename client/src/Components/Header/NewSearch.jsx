4d0b8822f6d61c980fd27c0c2cee3a25fb5d20e5
nguyentrung031 <trung9977.mta@gmail.com>
2019-11-24 21:29:31 +0700

    #11 fix indent

--- client/src/Components/Header/NewSearch.jsx
+++ client/src/Components/Header/NewSearch.jsx
@@ -3,35 +3,35 @@ import {Link} from "react-router-dom";
 import { connect } from 'react-redux';
 
 class NewSearch extends Component {
-	render() {
-		const {item} = this.props.location.state;
-		return (
-			<div className="container search-wrap">
-				{
-					item.map((value,key) => (
-						<ul className="list-unstyled" key={key}>
-							<li className="media">
-								<Link to={`/Detail/${value.id}`}>
-									<img src={value.img} className="mr-3 i1" alt={value.name} />
-								</Link>
-								<div className="media-body">
-									<Link to={`/Detail/${value.id}`}>
-										<h3 className="mt-0 mb-1">{value.name}</h3>
-									</Link>
-									<span>{value.date}</span>
-									<p>{value.content}</p>
-								</div>
-							</li>
-						</ul>
-					))
-				}
-			</div>
-		);
-	}
+  render() {
+    const {item} = this.props.location.state;
+    return (
+      <div className="container search-wrap">
+        {
+          item.map((value,key) => (
+            <ul className="list-unstyled" key={key}>
+              <li className="media">
+                <Link to={`/Detail/${value.id}`}>
+                  <img src={value.img} className="mr-3 i1" alt={value.name} />
+                </Link>
+                <div className="media-body">
+                  <Link to={`/Detail/${value.id}`}>
+                    <h3 className="mt-0 mb-1">{value.name}</h3>
+                  </Link>
+                  <span>{value.date}</span>
+                  <p>{value.content}</p>
+                </div>
+              </li>
+            </ul>
+          ))
+        }
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state, ownProps) => {
-	return {
-		List: state.List
-	}
+  return {
+    List: state.List
+  }
 }
 export default connect(mapStateToProps)(NewSearch);
